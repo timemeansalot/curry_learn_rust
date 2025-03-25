27415a4c19528733d3b22622eeed305019197e74
timemeansalot <timemeansalot@gmail.com>
2025-03-25 17:13:28 +0800

    add discription for unwrap_or and unwrap_or_else

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,11 @@
 /// ```
 use std::env;
 fn main() {
-    let flag = env::var("IS_FLAG").unwrap_or_else(|_| "false".to_string());
+    let default = "false".to_string();
+    // unwrap_or: return value in Ok(value) or return the default value provided
+    let flag = env::var("IS_FLAG").unwrap_or(default);
+    // unwrap_or_else: return value in Ok(value) or calculate the value from closure provided
+    //let flag = env::var("IS_FLAG").unwrap_or_else(|_| "false".to_string());
     if flag == "true" {
         println!("read true from env!");
     } else {
